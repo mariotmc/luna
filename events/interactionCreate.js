ce72ded71b4e4739ce90640f7db148d3d3bd36a0
mariotmc <mariotomicx@gmail.com>
2024-05-27 17:36:24 +0200

    handle other forms of command input

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,58 +4,65 @@ const { Collection } = require("discord.js");
 module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
-    if (!interaction.isChatInputCommand()) return;
+    if (interaction.isChatInputCommand()) {
+      const command = interaction.client.commands.get(interaction.commandName);
 
-    const command = interaction.client.commands.get(interaction.commandName);
+      if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+      }
 
-    if (!command) {
-      console.error(`No command matching ${interaction.commandName} was found.`);
-      return;
-    }
+      // AUTOCOMPLETE
+      if (interaction.isAutocomplete()) {
+        try {
+          await command.autocomplete(interaction);
+        } catch (error) {
+          console.error(error);
+        }
+      }
 
-    if (interaction.isAutocomplete()) {
+      // COOLDOWNS
+      const { cooldowns } = interaction.client;
+
+      if (!cooldowns.has(command.data.name)) {
+        cooldowns.set(command.data.name, new Collection());
+      }
+
+      const now = Date.now();
+      const timestamps = cooldowns.get(command.data.name);
+      const defaultCooldownDuration = 3;
+      const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+
+      if (timestamps.has(interaction.user.id)) {
+        const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+          const expiredTimestamp = Math.round(expirationTime / 1_000);
+          return interaction.reply({
+            content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
+            ephemeral: true,
+          });
+        }
+      }
+
+      timestamps.set(interaction.user.id, now);
+      setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+      // EXECUTE
       try {
-        await command.autocomplete(interaction);
+        await command.execute(interaction);
       } catch (error) {
         console.error(error);
+        if (interaction.replied || interaction.deferred) {
+          await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
+        } else {
+          await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+        }
       }
-    }
-
-    const { cooldowns } = interaction.client;
-
-    if (!cooldowns.has(command.data.name)) {
-      cooldowns.set(command.data.name, new Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.data.name);
-    const defaultCooldownDuration = 3;
-    const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
-
-    if (timestamps.has(interaction.user.id)) {
-      const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-      if (now < expirationTime) {
-        const expiredTimestamp = Math.round(expirationTime / 1_000);
-        return interaction.reply({
-          content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
-          ephemeral: true,
-        });
-      }
-    }
-
-    timestamps.set(interaction.user.id, now);
-    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
-
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      if (interaction.replied || interaction.deferred) {
-        await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
-      } else {
-        await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
-      }
+    } else if (interaction.isButton()) {
+      // respond to permanent button feature
+    } else if (interaction.isStringSelectMenu()) {
+      // respond to permanent select menu feature
     }
   },
 };
